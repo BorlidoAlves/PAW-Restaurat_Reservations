472a5c31e36bd97a8f6b675b6a1f2ad6101c046d
8160567 <8160567@estg.ipp.pt>
2020-09-09 18:20:59 +0100

    Frontend e API

--- Frontend/my-restaurant/src/app/list-menu-user/list-menu-user.component.ts
+++ Frontend/my-restaurant/src/app/list-menu-user/list-menu-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RestService } from '../rest.service';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-list-menu-user',
